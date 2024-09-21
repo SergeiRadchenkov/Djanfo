4ac72d63f5b4567c8072def0d68963f535f16fb1
SergeiRadchenkov <radchenkovsa@yandex.ru>
2024-09-21 10:07:36 +0300

    Lection 4

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'myapp',
     'myapp2',
     'myapp3',
+    'myapp4',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
